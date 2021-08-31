ffd027e84769797b6aad2ee1c90614f59f1ceb06
Kirill12a <kirilldrozd12@gmail.com>
2021-08-31 23:04:42 +0300

    Initial Commit

---
+++ changeChildName/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  changeChildName
+//
+//  Created by Kirill Drozdov on 31.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ changeChildName/changeChildNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  changeChildNameApp.swift
+//  changeChildName
+//
+//  Created by Kirill Drozdov on 31.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct changeChildNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
